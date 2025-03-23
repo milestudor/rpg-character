5828718036fe8b452e40ff849fe41cb2abda0d46
milestudor <mat6301@psu.edu>
2025-03-23 17:59:05 -0400

    class

--- rpg-character.js
+++ rpg-character.js
@@ -5,6 +5,7 @@
 import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
+import '@haxtheweb/rpg-character/rpg-character.js';
 
 /**
  * `rpg-character`
@@ -20,12 +21,11 @@ export class RpgCharacter extends DDDSuper(I18NMixin(LitElement)) {
 
   constructor() {
     super();
-    this.title = "";
-    this.login = "milestudor";
-    this.avatar = "https://github.com/${milestudor}.png";
-    this.getElementById("rpg-character").setAttribute("login", this.login);
-    this.getElementById("rpg-character").setAttribute("avatar", this.avatar);
-    this.p = this.login;
+    this.org = '';
+    this.repo = '';
+    this.title = '';
+    this.items = [];
+    this.limit = 15;
     this.t = this.t || {};
     this.t = {
       ...this.t,
@@ -44,7 +44,11 @@ export class RpgCharacter extends DDDSuper(I18NMixin(LitElement)) {
   static get properties() {
     return {
       ...super.properties,
+      org: { type: String },
+      repo: { type: String },
       title: { type: String },
+      items: { type: Array },
+      limit: { type: Number },
     };
   }
 
@@ -65,16 +69,52 @@ export class RpgCharacter extends DDDSuper(I18NMixin(LitElement)) {
       h3 span {
         font-size: var(--rpg-character-label-font-size, var(--ddd-font-size-s));
       }
+      .rpg-wrapper {
+        display: flex;
+        align-items: center;
+        margin: var(--ddd-spacing-2);
+      }
     `];
   }
 
+  updated(changedProperties) {
+    super.updated(changedProperties);
+    if (changedProperties.has('org') || changedProperties.has('repo')) {
+      this.getData();
+    }
+  }
+
+  getData() {
+    const url = `https://api.github.com/repos/${this.org}/${this.repo}/contributors`;
+    try {
+      fetch(url).then(d => d.ok ? data.json(): {}).then(data => {
+        if (data) {
+          this.items = [];
+          this.items = data;
+        }
+      });
+    } catch (error) {
+      console.error("Hello");
+    }
+  }
   // Lit render the HTML
   render() {
     return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
+    <div class="wrapper">
+      <h3>GitHub Repo: <a href="https://github.com/${this.org}/${this.repo}">${this.org}/${this.repo}</a></h3>
+    </div>;
+    <slot></slot>
+    ${this.items.filter((item, index) => index < this.limit).map((item) => 
+      html`
+      <div class="rpg-wrapper">
+      <rpg-character  seed="${item.login}"></rpg-character>
+        <div class="contributions">
+        <a href=https://github.com/${item.login}>${item.login}</a>
+        Contributions: ${item.contributions}
+        </div>
+      </div>
+     `)}
+  </div>`;
   }
 
   /**
